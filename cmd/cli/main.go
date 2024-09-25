ebacd09d1fd93e820867b1047a5d717c3b537897
Jonathan Munoz <madzumo@gmail.com>
2024-09-25 06:49:02 -0400

    add interval message

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -105,6 +105,7 @@ func menuSelection(menuSelect string, c *configSettings) {
 			}
 
 			if c.Interval > 0 {
+				fmt.Println(hp.LipSystemMsgStyle.Render(fmt.Sprintf("\nRepeat Interval in %dmin", c.Interval)))
 				time.Sleep(time.Duration(c.Interval) * time.Minute)
 			} else {
 				break
